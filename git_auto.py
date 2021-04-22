2ca337f3af79ecb0d295135f4e5c54b1d6b2d4b6
harshvardhanguleria <harshvardhanguleria@gmail.com>
2021-04-22 21:18:30 +0530

    Modified

--- git_auto.py
+++
@@ -1,4 +0,0 @@
-import os
-lst = ['git pull', 'git add -A', 'git commit -m "Modified"', 'git push']
-for i in lst:
-    os.system(i) 
---
+++ openAppsOnWSL.sh
@@ -0,0 +1,26 @@
+echo "1. IntelliJ"
+echo "2. Eclipse"
+echo "3. Java"
+echo "4. Python"
+echo "5. CPP"
+echo "6. C"
+echo "7. Chrome"
+
+read choice
+
+case $choice in
+    1) /mnt/c/JetBrains/'IntelliJ IDEA Community Edition 2021.1'/bin/idea64.exe
+    ;;
+    2) /mnt/c/EclipseJava/eclipse/eclipse.exe
+    ;;
+    3) cd /mnt/d/codes/Java ; code .
+    ;;
+    4) cd /mnt/d/codes/Python ; code .
+    ;;
+    5) cd /mnt/d/codes/CPP ; code .
+    ;;
+    6) cd /mnt/d/codes/C ; code .
+    ;;
+    7) cd /mnt/c/Program Files/Google/Chrome/Application/chrome.exe
+    ;;
+esac
